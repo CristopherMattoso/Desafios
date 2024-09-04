310058309df93a375906aa945de9235310438c54
CristopherMattoso <75039825+CristopherMattoso@users.noreply.github.com>
2024-09-04 08:51:35 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Desafios</title>
+    <style>
+      body {
+        text-align: center;
+        width: 70%;
+        margin: auto;
+      }
+      section {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+      }
+      h2 {
+        background-color: gray;
+      }
+    </style>
+  </head>
+  <body>
+    <h2>2024</h2>
+    <section>
+      <form action="1o Tri/1o Tri.php" method="post">
+        <button type="submit">1o Tri</button>
+      </form>
+      <form action="2o Tri/2o Tri.php" method="post">
+        <button type="submit">2o Tri</button>
+      </form>
+      <form action="3o Tri/3o Tri.php" method="post">
+        <button type="submit">3o Tri</button>
+      </form>
+    </section>
+    <br><br><br><br>
+  </body>
+</html>
